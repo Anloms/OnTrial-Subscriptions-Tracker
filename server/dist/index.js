d43520c0c37aff5f20f7a5a1508896cd33128e4e
Oweley <aleksandra.smolna@gmail.com>
2024-05-01 19:40:06 +0100

    investigating the Error R10

--- server/dist/index.js
+++ server/dist/index.js
@@ -12,7 +12,7 @@ const app = (0, express_1.default)();
 //newly added
 app.use((0, cors_1.default)());
 app.use(express_1.default.json());
-app.use(express_1.default.static(path_1.default.join(__dirname, '../client/dist')));
+app.use(express_1.default.static(path_1.default.join(__dirname, 'client/dist/assets/index-C223foxh.js')));
 app.use('/', router_1.default);
 app.use((err, req, res, next) => {
     console.error(err.stack);
