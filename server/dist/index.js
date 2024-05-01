426946bf8ad361c5068db03e24e27394e3731ed8
Oweley <aleksandra.smolna@gmail.com>
2024-05-01 19:34:13 +0100

    investigating the Error R10

--- server/dist/index.js
+++ server/dist/index.js
@@ -18,7 +18,7 @@ app.use((err, req, res, next) => {
     console.error(err.stack);
     res.status(500).send('Something broke!');
 });
-let port = process.env.PROD_PORT;
+let port = process.env.PORT;
 if (!port) {
     port = 8080;
 } else {
