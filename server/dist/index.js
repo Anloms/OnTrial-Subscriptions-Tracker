3cbcb7e59582b2e7a3f90b2f636fa5e4d5ed1e05
Oweley <aleksandra.smolna@gmail.com>
2024-05-09 16:15:15 +0100

    introduced changes/removed dist/recompiled/redeployed

--- server/dist/index.js
+++ server/dist/index.js
@@ -11,7 +11,7 @@ require("./scheduledTasks/subscriptionChecker");
 const app = (0, express_1.default)();
 app.use((0, cors_1.default)());
 app.use(express_1.default.json());
-app.use(express_1.default.static(path_1.default.join(__dirname, 'client/dist')));
+app.use(express_1.default.static(path_1.default.join(__dirname, '../client/dist')));
 app.use('/', router_1.default);
 let port = process.env.PORT;
 if (port === null || port === '') {
